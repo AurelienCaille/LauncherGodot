47e546f11b48c3f767c68cf2fc02c2ac577c7382
Aurelien <conspirations.contact@gmail.com>
2022-03-22 13:12:28 +0100

    Fix missing scene_to_launch settings from project file

--- addons/launcher/launcher.gd
+++ addons/launcher/launcher.gd
@@ -51,6 +51,7 @@ func _set_from_project_settings():
 	hud.theme = load(ProjectSettings.get_setting("launcher/graphics/theme_launcher"))
 	game_texture.texture = load(ProjectSettings.get_setting("launcher/graphics/texture_background"))
 	settings_menu_packed = load(ProjectSettings.get_setting("launcher/run/settings_scene"))
+	scene_to_launch = ProjectSettings.get_setting("launcher/run/main_scene")
 	
 	var is_using_rss = ProjectSettings.get_setting("launcher/rss/is_using_rss")
 	
